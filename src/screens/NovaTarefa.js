c8640742838fda2908f766f5e3cbc21d3854bf9c
tales.ferreira <talesmateus1999@hotmail.com>
2025-04-25 20:53:46 -0400

    Iniciada implementação da tela de nova tarefa

---
+++ src/screens/NovaTarefa.js
@@ -0,0 +1,35 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function NovaTarefa() {
+    return (
+        <View>
+            <View style={styles.cabecalho}>
+                <Text style={styles.titulo}>Adicionar Tarefa</Text>
+            </View>
+            <View>
+                
+            </View>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    cabecalho: {
+        backgroundColor: 'blue',
+        width: '100%',
+        height: 60,
+        flexDirection: 'row',
+        justifyContent: "center",
+        alignItems: 'center'
+    },
+    titulo: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    }
+});
