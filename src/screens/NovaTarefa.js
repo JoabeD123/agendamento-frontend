ee53f3b96ad62a5e7b507dd50b5f189bc43f32d4
tales.ferreira <talesmateus1999@hotmail.com>
2025-04-29 19:02:08 -0400

    Inicializada implementação do Picker

--- src/screens/NovaTarefa.js
+++ src/screens/NovaTarefa.js
@@ -1,4 +1,5 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
 export default function NovaTarefa() {
     return (
@@ -6,8 +7,25 @@ export default function NovaTarefa() {
             <View style={styles.cabecalho}>
                 <Text style={styles.titulo}>Adicionar Tarefa</Text>
             </View>
-            <View>
-                
+            <View style={styles.body}>
+                <Text style={styles.texto}>Nome da Tarefa:</Text>
+                <TextInput style={styles.textInput} />
+
+                <Text style={styles.texto}>Categotia da Tarefa:</Text>
+                <Picker style={styles.textInput}>
+                    <Picker.Item label="Estudo" value="estudo" />
+                    <Picker.Item label="Trabalho" value="trabalho" />
+                    <Picker.Item label="Reunião" value="reuniao" />
+                    <Picker.Item label="Prova" value="prova" />
+                    <Picker.Item label="Aula" value="aula" />
+                </Picker>
+
+                <Text style={styles.texto}>Descrição da Tarefa:</Text>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder='Value'
+                />
+
             </View>
         </View>
     )
@@ -31,5 +49,19 @@ const styles = StyleSheet.create({
         fontSize: 18,
         fontWeight: 'bold',
         textAlign: 'center'
+    },
+    body: {
+        padding: 15
+    },
+    texto: {
+        marginBottom: 5
+    },
+    textInput: {
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: '#ccc',
+        padding: 10,
+        backgroundColor: 'white',
+        marginBottom: 15
     }
 });
