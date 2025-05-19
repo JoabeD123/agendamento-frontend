1189ffc1b5ceebab054b96ea54f5fd82c57df324
joabe <joabecdias2006@gmail.com>
2025-05-19 19:14:02 -0400

    atualizado

--- src/screens/NovaTarefa.js
+++ src/screens/NovaTarefa.js
@@ -1,54 +1,57 @@
-import React from "react"
-import { View, Text, StyleSheet, useState} from "react-native"
-import { TextInput } from "react-native"
-import {Dropdown } from "react-native-element-dropdown"
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import { useNavigation } from '@react-navigation/native';
 
 export default function NovaTarefa() {
 
-    const data = [
-        { label: 'selecione uma categoria', value: '0' },
-        { label: 'estudos', value: '1' },
-        { label: 'trabalho', value: '2' },
-        { label: 'programação', value: '3' },
-        { label: 'reunião', value: '4' },
-      ];
+    const navigation = useNavigation();
+
     return (
-        <View >
+        <View>
             <View style={styles.cabecalho}>
-                <Text style={styles.titulo}>adicionar tarefa</Text>
-                        
+                <Text style={styles.titulo}>Adicionar Tarefa</Text>
             </View>
+            <View style={styles.body}>
+                <Text style={styles.texto}>Nome da Tarefa:</Text>
+                <TextInput style={styles.textInput} />
 
-            <View>
-                
-                <View style={styles.body}>
-                    <Text style={styles.texto}>Nome da tarefa:</Text>
-                    <TextInput style={styles.TextInput}/>
-                    <Text style={styles.texto}>Categoria da tarefa</Text>
-                    <Dropdown
-                    styles={styles.dropdown}
-                    data = {data}
-                    placeholderStyle={styles.placeholderStyle}
-                    selectedTextStyle={styles.selectedTextStyle}
-                    inputSearchStyle={styles.inputSearchStyle}
-                    placeholder="Selecione uma categoria"
-                    labelField={"label"}
-                    valueField={"value"}
-                    value= '0'
-                    search
-                    maxHeight={300}
-                    onChange={item => {
-                        console.log(item.value)
-                    }}
-                    />
-                               
+                <Text style={styles.texto}>Categotia da Tarefa:</Text>
+                <Picker style={styles.textInput}>
+                    <Picker.Item label="Estudo" value="estudo" />
+                    <Picker.Item label="Trabalho" value="trabalho" />
+                    <Picker.Item label="Reunião" value="reuniao" />
+                    <Picker.Item label="Prova" value="prova" />
+                    <Picker.Item label="Aula" value="aula" />
+                </Picker>
+
+                <Text style={styles.texto}>Descrição da Tarefa:</Text>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder='Value'
+                    multiline
+                    numberOfLines={3}
+                />
+
+                <TextInput 
+                    style={styles.textDate}
+                    placeholder='dd/mm/yyyy'
+                />
+
+                <View style={styles.containerBotao}>
+                    <TouchableOpacity style={styles.botao} onPress={() => navigation.goBack()}>
+                        <Text style={styles.botaoTexto}>Cancel</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.botao}>
+                        <Text style={styles.botaoTexto}>OK</Text>
+                    </TouchableOpacity>
                 </View>
-           
-            </View>
 
+            </View>
         </View>
     )
 }
+
 
 const styles = StyleSheet.create({
     container: {
@@ -63,63 +66,43 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     titulo: {
-        color: 'black',
+        color: 'white',
         fontSize: 18,
         fontWeight: 'bold',
-        textAlign: 'left'
-    },
-    TextInput: {
-        backgroundColor: 'white',
-        flex: 1,
-        height: 40,
-        borderRadius: 20,
-        borderColor: '#CCC',
-        borderWidth: 2,
-        padding: 5,
-        flex: 0.9
-    },
-    form: {
-        flexDirection: 'row',
-    },
-    textForm: {
-        color: 'black',
-        fontSize: 18,
-        fontWeight: 'bold',
-        flex: 0.1,
-
+        textAlign: 'center'
     },
     body: {
-        padding: 15
+        padding: 15,
     },
     texto: {
-        marginBottom:5
+        marginBottom: 5
     },
-    dropdown: {
-        marginTop: 100,
-        margin: 16,
-        heinght: 50,
-        border: 2,
-        borderColor: 'black',
-        borderBottomColor:'black',
-        borderbottomwidth: 0.5,
-        color: 'black'
+    textInput: {
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: '#ccc',
+        padding: 10,
+        backgroundColor: 'white',
+        marginBottom: 15
     },
-    placeholderStyle: {
-        fontSize: 16,
-        color: 'black'
+    textDate: {
+        height: 60,
+        borderWidth: 3,
+        borderColor: 'indigo',
+        borderRadius: 5,
+        margin: 40,
+        marginVertical: 30,
+        padding: 15,
+        backgroundColor: 'white'
     },
-    selectedTextStyle: {
-        fontSize: 16,
-        color: 'black'
-
+    containerBotao: {
+        flexDirection: 'row',
+        justifyContent: 'end'
     },
-    inputSearchStyle: {
-        height: 40,
-        fontSize: 16,
-        color: 'black'
+    botao: {
+        padding: 15
     },
-    
-    
-
-}
-)
+    botaoTexto: {
+        color: 'indigo'
+    }
+});
