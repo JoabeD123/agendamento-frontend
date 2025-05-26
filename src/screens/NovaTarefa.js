addd8caa4921236c0bdb9d93429a4a88df7b0969
joabe <joabecdias2006@gmail.com>
2025-05-26 19:20:36 -0400

    atualização

--- src/screens/NovaTarefa.js
+++ src/screens/NovaTarefa.js
@@ -1,10 +1,43 @@
 import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import { useNavigation } from '@react-navigation/native';
+import { useState } from 'react';
+
+import MaskInput from 'react-native-mask-input';
+import { addData } from '../storage/AsyncStorage';
 
 export default function NovaTarefa() {
 
     const navigation = useNavigation();
+
+    const [ nome, setNome ] = useState('')
+    const [ categotia, setCategotia ] = useState('prova')
+    const [ descricao, setDescricao ] = useState('')
+    const [ data, setData ] = useState('')
+
+    const handleSave = async () => {
+        const tarefa = {
+            nome: nome,
+            categoria: categotia,
+            data: data,
+            descricao: descricao
+        };
+
+        if (nome.trim() === '') {
+            alert('Por favor, insira um nome para a tarefa.');
+        }
+        else if (descricao.trim() === '') {
+            alert('Por favor, insira uma descrição para a tarefa.');
+        }
+        else if (data.trim() === '') {
+            alert('Por favor, insira uma data para a tarefa.');
+        }
+        else {
+            await addData(tarefa);
+            alert('Tarefa adicionada com sucesso!');
+            navigation.navigate('Home');
+        }
+    }
 
     return (
         <View>
@@ -13,10 +46,10 @@ export default function NovaTarefa() {
             </View>
             <View style={styles.body}>
                 <Text style={styles.texto}>Nome da Tarefa:</Text>
-                <TextInput style={styles.textInput} />
+                <TextInput style={styles.textInput} value={nome} onChangeText={texto => setNome(texto)} />
 
                 <Text style={styles.texto}>Categotia da Tarefa:</Text>
-                <Picker style={styles.textInput}>
+                <Picker style={styles.textInput} selectedValue={categotia} onValueChange={texto => setCategotia(texto)}>
                     <Picker.Item label="Estudo" value="estudo" />
                     <Picker.Item label="Trabalho" value="trabalho" />
                     <Picker.Item label="Reunião" value="reuniao" />
@@ -30,11 +63,14 @@ export default function NovaTarefa() {
                     placeholder='Value'
                     multiline
                     numberOfLines={3}
+                    value={descricao} onChangeText={texto => setDescricao(texto)}
                 />
 
-                <TextInput 
+                <MaskInput
                     style={styles.textDate}
                     placeholder='dd/mm/yyyy'
+                    value={data} onChangeText={texto => setData(texto)}
+                    mask={['(', /[1-9]/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/, ')']}
                 />
 
                 <View style={styles.containerBotao}>
@@ -42,7 +78,9 @@ export default function NovaTarefa() {
                         <Text style={styles.botaoTexto}>Cancel</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.botao}>
+                    <TouchableOpacity style={styles.botao} onPress={() => {
+                        handleSave()
+                    }}>
                         <Text style={styles.botaoTexto}>OK</Text>
                     </TouchableOpacity>
                 </View>
